aac2bf0f5a23fab8682c4c365877e9043dba982d
Charly1299 <carlos08solorio@gmail.com>
2026-06-12 17:32:10 -0600

    Se agregan funciones dinamicas en las imagenes para la  seccion de proyectos

--- js/script.js
+++ js/script.js
@@ -1,17 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-const elementos = document.querySelectorAll(".info-item");
+    const elementos = document.querySelectorAll(".info-item");
 
-function mostrarElementos() {
-    elementos.forEach((elemento) => {
-        const posicion = elemento.getBoundingClientRect().top;
-        const pantalla = window.innerHeight;
+    function mostrarElementos() {
+        elementos.forEach((elemento) => {
+            const posicion = elemento.getBoundingClientRect().top;
+            const pantalla = window.innerHeight;
 
-        if (posicion < pantalla - 100) {
-            elemento.classList.add("show");
-        }
+            if (posicion < pantalla - 100) {
+                elemento.classList.add("show");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", mostrarElementos);
+    mostrarElementos();
+
+    document.querySelectorAll(".proyecto").forEach(proyecto => {
+        proyecto.addEventListener("click", () => {
+
+            document.querySelectorAll(".proyecto").forEach(item => {
+                if(item !== proyecto){
+                    item.classList.remove("activo");
+                }
+            });
+
+            proyecto.classList.toggle("activo");
+        });
     });
-}
 
-window.addEventListener("scroll", mostrarElementos);
+});
 
-mostrarElementos();
+const visor = document.getElementById("visor-imagen");
+const imagenGrande = document.getElementById("imagen-grande");
+
+document.querySelectorAll(".imagenes-extra img").forEach(img => {
+
+    img.addEventListener("mouseenter", () => {
+        imagenGrande.src = img.src;
+        visor.classList.add("activo");
+    });
+
+});
+
+visor.addEventListener("mouseleave", () => {
+    visor.classList.remove("activo");
+});
